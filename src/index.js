4ea93d33b41c0a7561ac012ba5cfa5c9a74da368
VLADON4UK <drugdobryaka@gmail.com>
2023-02-10 17:45:09 +0300

    solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let nString = String(n);
+  let nArray = nString.split('');
+  if (n < 0){
+    nArray.shift();
+  }
+  let result = '';
+  for (number of nArray){
+    result = number + result;
+  }
+  return Number(result);
 }
